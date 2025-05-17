0dd7dd525e1c017e6e917d74562681b35fad6f58
GruheshKurra <gruheshkurra2@gmail.com>
2025-05-17 09:29:58 +0530

    Your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Chat from "./pages/Chat";
-import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "./components/ThemeProvider";
 
 const queryClient = new QueryClient();
@@ -20,7 +19,6 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/chat" element={<Chat />} />
-            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
--- src/components/BotIcon.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import { Bot } from 'lucide-react';
-
-export const BotIcon = ({ className }: { className?: string }) => {
-  return <Bot className={className} />;
-};
